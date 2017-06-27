6fa08cd54217e87c9fcc7247aed5e524b7d64425
Administrator <Administrator@WIN-M094ETU8CF9>
2017-06-27 20:38:07 +0900

    test

--- Mini_Pjt/src/Model/member/Vo/MemberVo.java
+++ Mini_Pjt/src/Model/member/Vo/MemberVo.java
@@ -1,5 +1,15 @@
 package Model.member.Vo;
 
 public class MemberVo {
+	private String id;
+	
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getId() {
+		return id;
+	}
 
 }
